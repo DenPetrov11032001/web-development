f4c5f78c54f83bcdfee08470b187c2f05cdd2450
DenPetrov11032001 <den11032001@mail.ru>
2020-02-28 22:09:22 +0300

    lw3

--- lw3/task4.php
+++ lw3/task4.php
@@ -9,12 +9,14 @@
     $dir = 'data/'; 
     if (!file_exists($dir.$emailUser)){ 
         $fileSource = fopen($dir.$emailUser.'.txt', 'w+'); 
-        fwrite($fileSource, $emailUser.PHP_EOL);
     }
 
     $firstNameUser = getGETParameter('first_name');
-    $lastName = getGETParameter('last_name');
+    $lastNameuser = getGETParameter('last_name');
     $ageUser = getGETParameter('age');
     if ($firstNameUser != null) fwrite($fileSource, $firstNameUser.PHP_EOL);
-    if ($lastName != null) fwrite($fileSource, $lastName.PHP_EOL);
+    if ($lastNameuser != null) fwrite($fileSource, $lastNameuser.PHP_EOL);
+    fwrite($fileSource, $emailUser.PHP_EOL);
     if ($ageUser != null) fwrite($fileSource, $ageUser.PHP_EOL);
+
+    fclose($fileSource);
