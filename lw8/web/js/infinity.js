3c2ff70dc887656005d485c3921e3e867eab5e05
DenPetrov11032001 <den11032001@mail.ru>
2020-04-18 21:18:13 +0300

    fix

--- lw8/web/js/infinity.js
+++ lw8/web/js/infinity.js
@@ -1,47 +1,26 @@
-function changeNextDiv(id) {
-  let curr = document.getElementById(id);
-  let next = document.getElementById(++id);
-
-  let currClone = curr.cloneNode(true);
-  let nextClone = next.cloneNode(true);
-
-  next.parentNode.insertBefore(currClone, next);
-  curr.parentNode.insertBefore(nextClone, curr);
-  curr.parentNode.removeChild(curr);
-  next.parentNode.removeChild(next);
-}
-
-function changePrevDiv(id) {
-  let curr = document.getElementById(id);
-  let prev = document.getElementById(--id);
-
-  let currClone = curr.cloneNode(true);
-  let prevClone = prev.cloneNode(true);
-
-  prev.parentNode.insertBefore(currClone, prev);
-  curr.parentNode.insertBefore(prevClone, curr);
-  curr.parentNode.removeChild(curr);
-  prev.parentNode.removeChild(prev);
-}
-
 function sliderNext() {
-  for (let i = 0; i < 9; i++) {
-    changeNextDiv(i);
-  }
+  let slider = document.getElementById('slider_wrapper');
+  let sliderLength = slider.children.length;
+  let lastElement = slider.children[sliderLength - 1];
+
+  lastElement.parentNode.removeChild(lastElement);
+  slider.prepend(lastElement);
 }
 
 function sliderPrev() {
-  for (let i = 9; i > 0; i--) {
-    changePrevDiv(i);
-  }
+  let slider = document.getElementById('slider_wrapper');
+  let firstElement = slider.children[0];
+
+  firstElement.parentNode.removeChild(firstElement);
+  slider.append(firstElement);
 }
 
 function run() {
-  const next = document.getElementById('next');
-  next.addEventListener('click', sliderNext);
-
   const prev = document.getElementById('prev');
   prev.addEventListener('click', sliderPrev);
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', sliderNext);
 }
 
 window.onload = run;
