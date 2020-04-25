1bb3b0b75d2fbb181636905202f103e507143ab6
DenPetrov11032001 <den11032001@mail.ru>
2020-04-25 21:19:02 +0300

    ver4

--- lw9/web/js/form.js
+++ lw9/web/js/form.js
@@ -1,5 +1,5 @@
 
-function checkEmail(email, name) {
+function checkData(email, name) {
   let data = new FormData();
   data.append("email", email);
   data.append("name", name);
@@ -18,43 +18,43 @@ function checkEmail(email, name) {
 async function dataValidation() {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
-  let dataValid = await checkEmail(email, name);
+  const dataValid = await checkData(email, name);
 
   const isEmailValid = dataValid.email;
   const isNameValid = dataValid.name;
 
-   const message = document.getElementById('message').value;
+  const message = document.getElementById('message').value;
 
-   let cellName = document.querySelector('.input_cell_name');
-   if (isNameValid) {
-     cellName.style.border = '2px solid #c4c4c4';
-   } else {
-     cellName.style.border = '2px solid #EE5252';
-   }
+  let cellName = document.querySelector('.input_cell_name');
+  if (isNameValid) {
+    cellName.style.border = '2px solid #c4c4c4';
+  } else {
+    cellName.style.border = '2px solid #EE5252';
+  }
 
-   let cellEmail = document.querySelector('.input_cell_email');
-   if (isEmailValid) {
-     cellEmail.style.border = '2px solid #c4c4c4';
-   } else {
-     cellEmail.style.border = '2px solid #EE5252';
-   }
+  let cellEmail = document.querySelector('.input_cell_email');
+  if (isEmailValid) {
+    cellEmail.style.border = '2px solid #c4c4c4';
+  } else {
+    cellEmail.style.border = '2px solid #EE5252';
+  }
 
-   let cellMessage = document.querySelector('.input_cell_text');
-   if (message === '') {
-     cellMessage.style.border = '2px solid #EE5252';
-   } else {
-     cellMessage.style.border = '2px solid #c4c4c4';
-   }
+  let cellMessage = document.querySelector('.input_cell_text');
+  if (message === '') {
+    cellMessage.style.border = '2px solid #EE5252';
+  } else {
+    cellMessage.style.border = '2px solid #c4c4c4';
+  }
 
-   let checkMark = document.querySelector('.check_mark');
-   let messageTrue = document.querySelector('.message_true_form');
-   if (isNameValid && isEmailValid && (message !== '')) {
-     checkMark.style.visibility = 'visible';
-     messageTrue.style.visibility = 'visible';
-   } else {
-     checkMark.style.visibility = 'hidden';
-     messageTrue.style.visibility = 'hidden';
-   }
+  let checkMark = document.querySelector('.check_mark');
+  let messageTrue = document.querySelector('.message_true_form');
+  if (isNameValid && isEmailValid && (message !== '')) {
+    checkMark.style.visibility = 'visible';
+    messageTrue.style.visibility = 'visible';
+  } else {
+    checkMark.style.visibility = 'hidden';
+    messageTrue.style.visibility = 'hidden';
+  }
 }
 
 function run() {
