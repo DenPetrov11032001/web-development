8fecc8e0eb28065844b58b55548c7b4efbf0c93f
DenPetrov11032001 <den11032001@mail.ru>
2020-04-25 21:42:37 +0300

    ver5.2

--- lw9/web/js/form.js
+++ lw9/web/js/form.js
@@ -27,64 +27,44 @@ async function dataValidation() {
 
   const cellName = document.getElementById('name');
   if (isNameValid) {
-    if(cellName.classList.contains('input_data_false')) {
-      cellName.classList.remove('input_data_false');
-    }
+    cellName.classList.remove('input_data_false');
     cellName.classList.add('input_data_true');
   } else {
-    if(cellName.classList.contains('input_data_true')) {
-      cellName.classList.remove('input_data_true');
-    }
+    cellName.classList.remove('input_data_true');
     cellName.classList.add('input_data_false');
   }
 
   const cellEmail = document.getElementById('email');
   if (isEmailValid) {
-    if(cellEmail.classList.contains('input_data_false')) {
-      cellEmail.classList.remove('input_data_false');
-    }
+    cellEmail.classList.remove('input_data_false');
     cellEmail.classList.add('input_data_true');
   } else {
-    if(cellEmail.classList.contains('input_data_true')) {
-      cellEmail.classList.remove('input_data_true');
-    }
+    cellEmail.classList.remove('input_data_true');
     cellEmail.classList.add('input_data_false');
   }
 
   const cellMessage = document.getElementById('message');
   if (message !== '') {
-    if(cellMessage.classList.contains('input_data_false')) {
-      cellMessage.classList.remove('input_data_false');
-    }
+    cellMessage.classList.remove('input_data_false');
     cellMessage.classList.add('input_data_true');
   } else {
-    if(cellMessage.classList.contains('input_data_true')) {
-      cellMessage.classList.remove('input_data_true');
-    }
+    cellMessage.classList.remove('input_data_true');
     cellMessage.classList.add('input_data_false');
   }
 
   const checkMark = document.getElementById('check_mark');
   const messageTrue = document.getElementById('you_message_true');
   if (isNameValid && isEmailValid && (message !== '')) {
-    if(checkMark.classList.contains('data_visibility_false')) {
-      checkMark.classList.remove('data_visibility_false');
-    }
+    checkMark.classList.remove('data_visibility_false');
     checkMark.classList.add('data_visibility_true');
 
-    if(messageTrue.classList.contains('data_visibility_false')) {
-      messageTrue.classList.remove('data_visibility_false');
-    }
+    messageTrue.classList.remove('data_visibility_false');
     messageTrue.classList.add('data_visibility_true');
   } else {
-    if(checkMark.classList.contains('data_visibility_true')) {
-      checkMark.classList.remove('data_visibility_true');
-    }
+    checkMark.classList.remove('data_visibility_true');
     checkMark.classList.add('data_visibility_false');
 
-    if(messageTrue.classList.contains('data_visibility_true')) {
-      messageTrue.classList.remove('data_visibility_true');
-    }
+    messageTrue.classList.remove('data_visibility_true');
     messageTrue.classList.add('data_visibility_false');
   }
 }
